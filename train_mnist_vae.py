338d35e8e769dac12257152df7273887c3f2f70e
Lewis Smith <lewis.smith@eng.ox.ac.uk>
2018-03-19 20:15:53 +0000

    fixed some formatting

--- train_mnist_vae.py
+++ train_mnist_vae.py
@@ -1,4 +1,3 @@
-
 import keras
 from keras import backend as K
 from keras.layers import Dense, Flatten, Lambda, Reshape
@@ -6,32 +5,33 @@ from keras.models import Model
 
 import src.utilities as U
 
-BATCH_SIZE=128
+BATCH_SIZE = 128
 
 
 def define_VAE(optim='adagrad', latent_dim=2):
-    inputs = keras.layers.Input(shape=(28,28,1))
+    inputs = keras.layers.Input(shape=(28, 28, 1))
     x = Flatten()(inputs)
     enc_1 = Dense(400, activation='elu')(x)
-    enc_2 = Dense(256, activation = 'elu')(enc_1)
+    enc_2 = Dense(256, activation='elu')(enc_1)
 
     z_mu = Dense(latent_dim)(enc_2)
     z_logsigma = Dense(latent_dim)(enc_2)
 
-    encoder = Model(inputs=inputs, outputs=z_mu) #represent the latent space by the mean
+    encoder = Model(inputs=inputs, outputs=z_mu)  # represent the latent space by the mean
 
     def sample_z(args):
         mu, logsigma = args
-        return  0.5 * K.exp(logsigma / 2) * K.random_normal(shape=(K.shape(mu)[0], latent_dim)) + mu
-    z = Lambda(sample_z, output_shape=(latent_dim,))([z_mu, z_logsigma]) 
+        return 0.5 * K.exp(logsigma / 2) * K.random_normal(shape=(K.shape(mu)[0], latent_dim)) + mu
+
+    z = Lambda(sample_z, output_shape=(latent_dim,))([z_mu, z_logsigma])
 
     dec_input = keras.layers.Input(shape=(latent_dim,))
     dec_1 = Dense(256, activation='elu')(dec_input)
     dec_2 = Dense(400, activation='elu')(dec_1)
-    dec_output = Dense(784, activation='sigmoid')(dec_2) 
+    dec_output = Dense(784, activation='sigmoid')(dec_2)
 
-    dec_reshaped = Reshape((28,28,1))(dec_output)
-    decoder = Model(inputs=dec_input,outputs=dec_reshaped)
+    dec_reshaped = Reshape((28, 28, 1))(dec_output)
+    decoder = Model(inputs=dec_input, outputs=dec_reshaped)
 
     reconstruction = decoder(z)
 
@@ -43,10 +43,11 @@ def define_VAE(optim='adagrad', latent_dim=2):
         x_ent = keras.metrics.binary_crossentropy(x, rec)
         kl_div = 0.5 * K.sum(K.exp(z_logsigma) + K.square(z_mu) - z_logsigma - 1, axis=-1)
         return 28 * 28 * x_ent + kl_div
-    
+
     VAE.compile(optimizer=optim, loss=vae_loss)
 
     return VAE, encoder, decoder
+
 
 if __name__ == '__main__':
     latent_dim = 2
@@ -60,7 +61,7 @@ if __name__ == '__main__':
     VAE.fit(x_train, x_train,
             epochs=50,
             batch_size=BATCH_SIZE,
-            validation_data=(x_test,x_test))
+            validation_data=(x_test, x_test))
 
     encoder.save_weights('save/enc_weights_latent_dim_' + str(latent_dim) + '.h5')
     decoder.save_weights('save/dec_weights_latent_dim_' + str(latent_dim) + '.h5')
